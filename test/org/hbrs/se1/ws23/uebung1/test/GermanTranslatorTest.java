ab66af7d4d695ef825cf60aca7088217844d9c7e
lukas.neft <lukas.neft@smail.inf.h-brs.de>
2023-10-18 07:59:44 +0200

    Tests hinzugefügt

--- test/org/hbrs/se1/ws23/uebung1/test/GermanTranslatorTest.java
+++ test/org/hbrs/se1/ws23/uebung1/test/GermanTranslatorTest.java
@@ -8,12 +8,43 @@ import static org.junit.jupiter.api.Assertions.*;
 class GermanTranslatorTest {
 
     @Test
-    void aPositiveTest() {
+    void gAEK1Test() {
         GermanTranslator translator = new GermanTranslator();
-        String value = translator.translateNumber(0);
-        assertEquals(value, "null");
+        String value = translator.translateNumber(1);
+        assertEquals(value, "eins");
+    }
+    @Test
+    void uAEK1Test() {
+        GermanTranslator translator = new GermanTranslator();
+        String value = translator.translateNumber(20);
+        assertEquals(value, "Übersetzung der Zahl 20 nicht möglich (1.0)");
+    }
+    @Test
+    void uAEK2Test() {
+        GermanTranslator translator = new GermanTranslator();
+        String value = translator.translateNumber(-10);
+        assertEquals(value, "Übersetzung der Zahl -10 nicht möglich (1.0)");
     }
 
+    @Test
+    void SonderfallTest() {
+        GermanTranslator translator = new GermanTranslator();
+        String value = translator.translateNumber(0);
+        assertEquals(value, "Übersetzung der Zahl 0 nicht möglich (1.0)");
+    }
+
+    @Test
+    void GrenzwertTest() {
+        GermanTranslator translator = new GermanTranslator();
+        String value = translator.translateNumber(Integer.MAX_VALUE);
+        assertEquals(value, "Übersetzung der Zahl 2147483647 nicht möglich (1.0)");
+    }
+    @Test
+    void Grenzwert2Test() {
+        GermanTranslator translator = new GermanTranslator();
+        String value = translator.translateNumber(Integer.MIN_VALUE);
+        assertEquals(value, "Übersetzung der Zahl -2147483648 nicht möglich (1.0)");
+    }
 
 
 }
