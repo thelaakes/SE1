ff8c3d6a9cb9279a38b6a5dd3e115d56db30a4c0
lukas.neft <lukas.neft@smail.inf.h-brs.de>
2023-11-07 22:30:01 +0100

    dump Methode in MemberView verschoben

--- src/org/hbrs/se1/ws23/uebung3/view/MemberView.java
+++ src/org/hbrs/se1/ws23/uebung3/view/MemberView.java
@@ -1,4 +1,16 @@
 package org.hbrs.se1.ws23.uebung3.view;
 
+import org.hbrs.se1.ws23.uebung3.control.Container;
+import org.hbrs.se1.ws23.uebung3.control.Member;
+
+import java.util.ArrayList;
+
 public class MemberView {
+
+    // Gibt alle Member-Objekte im Container aus
+    public void dump(ArrayList<Member> list) {
+        for (Member member : list) {
+            System.out.println(member.toString());
+        }
+    }
 }
