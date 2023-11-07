c78c574759fe9f633cd130dd5da8c93f6bbeeb46
lukas.neft <lukas.neft@smail.inf.h-brs.de>
2023-11-07 22:31:54 +0100

    Kopie aus Übung2

---
+++ src/org/hbrs/se1/ws23/uebung3/control/ConcreteMember.java
@@ -0,0 +1,16 @@
+package org.hbrs.se1.ws23.uebung3.control;
+
+public class ConcreteMember implements Member {
+    private Integer id;
+    public ConcreteMember(Integer id) {
+        this.id = id;
+    }
+    @Override
+    public Integer getID() {
+        return id;
+    }
+    @Override
+    public String toString() {
+        return "Member (ID = " + id + ")";
+    }
+}
