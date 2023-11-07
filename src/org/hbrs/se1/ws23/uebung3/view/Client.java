94e2116599ca0265f98bedd32bc79a3deaccef51
lukas.neft <lukas.neft@smail.inf.h-brs.de>
2023-11-07 22:31:16 +0100

    Client Klasse hinzugefügt, diese fügt dem Container Objekt Member hinzu und ruft den view dump auf

--- src/org/hbrs/se1/ws23/uebung3/view/Client.java
+++ src/org/hbrs/se1/ws23/uebung3/view/Client.java
@@ -1,5 +1,16 @@
 package org.hbrs.se1.ws23.uebung3.view;
 
-public class Client {
+import org.hbrs.se1.ws23.uebung3.control.ConcreteMember;
+import org.hbrs.se1.ws23.uebung3.control.Container;
+import org.hbrs.se1.ws23.uebung3.control.ContainerException;
 
+public class Client {
+    public Client(Container container, MemberView view) throws ContainerException {
+
+        for (int i = 0; i < 10; i++) {
+            ConcreteMember member = new ConcreteMember(i);
+            container.addMember(member);
+        }
+        view.dump(container.getCurrentList());
+    }
 }
