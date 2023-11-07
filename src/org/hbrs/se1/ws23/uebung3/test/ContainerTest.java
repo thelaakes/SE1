8e5c5bd629b3143889702f736a4c8143327c4fc5
lukas.neft <lukas.neft@smail.inf.h-brs.de>
2023-11-07 22:32:30 +0100

    Initiale Testklasse für den Test der PersistenceStrategy

---
+++ src/org/hbrs/se1/ws23/uebung3/test/ContainerTest.java
@@ -0,0 +1,16 @@
+package org.hbrs.se1.ws23.uebung3.test;
+
+import org.hbrs.se1.ws23.uebung3.control.ConcreteMember;
+import org.hbrs.se1.ws23.uebung3.control.Container;
+import org.hbrs.se1.ws23.uebung3.control.ContainerException;
+import org.hbrs.se1.ws23.uebung3.control.Member;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ContainerTest {
+
+
+}
