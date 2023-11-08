e3da102baa77f4e20261a55152b9fc6b72a7f71e
lukas.neft <lukas.neft@smail.inf.h-brs.de>
2023-11-08 09:25:31 +0100

    PersistenceStrategy hinzugefügt

--- src/org/hbrs/se1/ws23/uebung3/control/Container.java
+++ src/org/hbrs/se1/ws23/uebung3/control/Container.java
@@ -7,16 +7,17 @@ import java.util.Objects;
 public class Container {
     private final ArrayList<Member> list = new ArrayList<>();
     private static Container instance = null;
+    private PersistenceStrategy<Member> ps = new PersistenceStrategyStream<>();
 
     // Singleton-Methode, gibt die einzige Instanz des Containers zurück oder erstellt sie
     public static Container getInstance() {
         if (instance == null) {
-            instance = new Container();
+            instance = new Container(new PersistenceStrategyStream<Member>());
         }
         return instance;
     }
 
-    private Container() {
+    private Container(PersistenceStrategy<Member> ps) {
         // Privater Konstruktor, löscht die Liste der Member-Objekte bei der Initialisierung
         this.list.clear();
     }
@@ -32,6 +33,10 @@ public class Container {
 
         // Wenn keine Duplikate gefunden wurden, füge das Member-Objekt zur Liste hinzu
         list.add(member);
+    }
+
+    public void setPersistenceStrategy(PersistenceStrategy<Member> ps) {
+        this.ps = ps;
     }
 
     // Löscht ein Member-Objekt aus dem Container anhand seiner ID
@@ -56,12 +61,10 @@ public class Container {
     }
 
     public void store() throws PersistenceException {
-        PersistenceStrategy<Member> ps = new PersistenceStrategyStream<Member>();
         ps.save(list);
     }
 
     public void load() throws PersistenceException, ContainerException {
-        PersistenceStrategy<Member> ps = new PersistenceStrategyStream<Member>();
         Container container = Container.getInstance();
         this.list.clear();
         for (
