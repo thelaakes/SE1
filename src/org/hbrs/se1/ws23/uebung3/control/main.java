31c4c6c6aad1b6abd1571c91f9a5fecbb3c0769e
lukas.neft <lukas.neft@smail.inf.h-brs.de>
2023-11-08 09:15:53 +0100

    main added

---
+++ src/org/hbrs/se1/ws23/uebung3/control/main.java
@@ -0,0 +1,21 @@
+package org.hbrs.se1.ws23.uebung3.control;
+
+import org.hbrs.se1.ws23.uebung3.control.ConcreteMember;
+import org.hbrs.se1.ws23.uebung3.control.Container;
+import org.hbrs.se1.ws23.uebung3.control.ContainerException;
+import org.hbrs.se1.ws23.uebung3.persistence.PersistenceException;
+import org.hbrs.se1.ws23.uebung3.view.Client;
+import org.hbrs.se1.ws23.uebung3.view.MemberView;
+
+public class main {
+    public static void main(String[] args) throws ContainerException, PersistenceException {
+        MemberView view = new MemberView();
+        Container container = Container.getInstance();
+        Client client = new Client();
+        container.addMember(new ConcreteMember(55));
+        container.load();
+        //container.addMember(new ConcreteMember(1));
+        client.viewDump();
+        //container.store();
+    }
+}
