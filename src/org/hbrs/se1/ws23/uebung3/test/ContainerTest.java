5c685c772a777fafd74edda8212c0356adf6a05c
lukas.neft <lukas.neft@smail.inf.h-brs.de>
2023-11-08 09:25:45 +0100

    TestCases not yet implemented

--- src/org/hbrs/se1/ws23/uebung3/test/ContainerTest.java
+++ src/org/hbrs/se1/ws23/uebung3/test/ContainerTest.java
@@ -12,5 +12,20 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 public class ContainerTest {
 
+    @BeforeEach
+    public void setup() {
+        Container container = Container.getInstance();
+    }
 
+    /*Entwickeln sie einen JUnit5-Testfall mit mindestens 4 aussagekräftigen TestMethoden, um ihre Klasse Container hinreichend zu testen. Folgende Testfälle sollten
+    dabei zumindest getestet werden (jeder Testfall eine Test-Methode):
+    • Keine Strategie von außen gesetzt („Test auf NULL“)
+    • Verwendung der Strategie „PersistenceStrategyMongoDB“
+    • Fehlerhafte Location des Files, in dem Streams ihre Objekte abspeichern (Tipp:
+    FileStreams mögen keine Directories ;-))
+    • „Round-Trip-Test“: Objekt hinzufügen, Liste persistent abspeichern, Objekt aus
+    Container löschen und Liste wieder einladen. Nach jedem Schritt den Zustand des
+    Containers testen!*/
+
+    //Tests sind noch nicht implementiert
 }
